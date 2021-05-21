30d68852edeaf5786d7ae88b5758d7f07d5c113c
vlymar90 <7ua.lymar2014@gmail.com>
2021-05-21 16:15:48 +0300

    init project

---
+++ android/src/com/gb/lymar/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gb.lymar;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gb.lymar.SpaceStar;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceStar(), config);
+	}
+}
---
+++ desktop/src/com/gb/lymar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gb.lymar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gb.lymar.SpaceStar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceStar(), config);
+	}
+}
