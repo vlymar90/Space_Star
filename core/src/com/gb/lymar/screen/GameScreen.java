8d2524f3395fdf6b0df8ee9a5bbbd7686fd24df9
vlymar90 <7ua.lymar2014@gmail.com>
2021-06-10 00:44:29 +0300

    task6

--- core/src/com/gb/lymar/screen/GameScreen.java
+++ core/src/com/gb/lymar/screen/GameScreen.java
@@ -3,8 +3,6 @@ package com.gb.lymar.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
@@ -14,9 +12,12 @@ import com.gb.lymar.math.Rect;
 import com.gb.lymar.pool.BulletPool;
 import com.gb.lymar.pool.EnemyPool;
 import com.gb.lymar.sprite.Background;
+import com.gb.lymar.sprite.EnemyShip;
 import com.gb.lymar.sprite.MainShip;
 import com.gb.lymar.sprite.Star;
 import com.gb.lymar.utils.EnemyEmitter;
+
+import java.util.List;
 
 public class GameScreen extends BaseScreen {
     private static final int STAR_COUNT = 64;
@@ -61,6 +62,7 @@ public class GameScreen extends BaseScreen {
     @Override
     public void render(float delta) {
         update(delta);
+        collisionShip();
         freeAllDestroyed();
         draw();
     }
@@ -137,5 +139,14 @@ public class GameScreen extends BaseScreen {
         bulletPool.drawActiveSprites(batch);
         enemyPool.drawActiveSprites(batch);
         batch.end();
+    }
+
+    private void collisionShip() {
+        List<EnemyShip> enemyShips = enemyPool.getActiveObjects();
+        for (EnemyShip c: enemyShips) {
+            if(mainShip.pos == c.pos) {
+                c.isOutside(c);
+            }
+        }
     }
 }
